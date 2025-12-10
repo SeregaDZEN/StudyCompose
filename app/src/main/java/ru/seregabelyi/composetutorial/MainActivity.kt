96e40d838b38dc944e05de5c2eb70e8c8d02f8f8
СерёгаБелый <fezarikus@gmail.com>
2025-12-10 21:05:23 +0300

    изменил положение текста и кнопки

--- app/src/main/java/ru/seregabelyi/composetutorial/MainActivity.kt
+++ app/src/main/java/ru/seregabelyi/composetutorial/MainActivity.kt
@@ -6,16 +6,27 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -62,11 +73,28 @@ fun PreviewMainScreen() {
 
 @Composable
 fun MainActivityContent() {
-    Column {
-        Header(R.drawable.sunrise, "sun image")
-        TemperatureText(0)
-    }
+    var celsius by rememberSaveable { mutableIntStateOf(0) } // rememberSaveable - сохр данные при изм конфиг
+    var newCelsius by rememberSaveable { mutableStateOf("") }
 
+    Column( modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally)
+     {
+        Header(R.drawable.sunrise, "sun image")
+        EnterTemperature(newCelsius) { newCelsius = it }
+        ConvertButton {
+            newCelsius.toIntOrNull()?.let {
+                celsius = it
+            }
+        }
+
+
+    }
+}
+
+@Composable
+fun ConvertButton(clicked: () -> Unit) {
+    Button(onClick = clicked) {
+        Text("Convert")
+    }
 }
 
 @Composable
@@ -85,10 +113,29 @@ fun TemperatureText(celsius: Int) {
     val fahrenheit = (celsius.toDouble()) * 9 / 5 + 32
     Text("$celsius is $fahrenheit Fahrenheit", color = Green, fontSize = 20.sp)
 }
+
+
 @Composable
-fun ConvertButton(clicked :  () -> Unit) {
-    Button(onClick = clicked) {
-        Text("Convert")
+fun ChangeHello() {
+    var name by remember { mutableStateOf("friend") }
+
+    Column {
+        Text("Hello, $name!", color = Blue)
+        Button(onClick = { name = "Compose" }) {
+            Text("Change")
+        }
     }
+
+}
+
+@Composable
+fun EnterTemperature(temp: String, changed: (String) -> Unit) {
+    TextField(
+        value = temp,
+        label = { Text("Enter a temperature in Celsius") },
+        onValueChange = changed,
+        textStyle = TextStyle(textAlign = TextAlign.Center),
+        modifier = Modifier.fillMaxWidth()
+    )
 }
 
