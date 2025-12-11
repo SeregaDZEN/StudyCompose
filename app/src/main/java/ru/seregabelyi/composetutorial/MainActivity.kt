3d74a054b01f42f6f80f72322e31ad88c4a337fa
СерёгаБелый <fezarikus@gmail.com>
2025-12-11 16:11:19 +0300

    удалил лишние компоненты + добавил тему

--- app/src/main/java/ru/seregabelyi/composetutorial/MainActivity.kt
+++ app/src/main/java/ru/seregabelyi/composetutorial/MainActivity.kt
@@ -4,23 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
-import androidx.compose.runtime.setValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -30,54 +28,30 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import ru.seregabelyi.composetutorial.ui.theme.Blue
 import ru.seregabelyi.composetutorial.ui.theme.Green
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MainActivityContent()
+            MaterialTheme {
+                Surface { MainActivityContent() }
+            }
         }
     }
 }
-
-@Composable
-fun MainScreen() {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        MessageCard(listOf("a", "b", "c"))
-    }
-}
-
-@Composable
-fun MessageCard(names: List<String>) {
-    names.forEach { name ->
-        Text(
-            "привет, $name!",
-            color = Blue,    // сам добавил ))
-            fontSize = 16.sp
-        )
-    }
-}
-
-@Preview
-@Composable
-fun PreviewMainScreen() {
-    MainActivityContent() // ←  для превью, нельзя с аргументами!
-}
-
 
 @Composable
 fun MainActivityContent() {
     var celsius by rememberSaveable { mutableIntStateOf(0) } // rememberSaveable - сохр данные при изм конфиг
     var newCelsius by rememberSaveable { mutableStateOf("") }
 
-    Column( modifier = Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally)
-     {
+    Column(
+        modifier = Modifier
+            .padding(16.dp)
+            .fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally
+    )
+    {
         Header(R.drawable.sunrise, "sun image")
         EnterTemperature(newCelsius) { newCelsius = it }
         ConvertButton {
@@ -85,8 +59,26 @@ fun MainActivityContent() {
                 celsius = it
             }
         }
+        TemperatureText(celsius)
+    }
+}
 
+@Composable
+fun EnterTemperature(temp: String, changed: (String) -> Unit) {
+    TextField(
+        value = temp,
+        label = { Text("Enter a temperature in Celsius") },
+        onValueChange = changed,
+        textStyle = TextStyle(textAlign = TextAlign.Center),
+        modifier = Modifier.fillMaxWidth()
+    )
+}
 
+@Preview
+@Composable
+fun PreviewMainScreen() {
+    MaterialTheme {
+        Surface { MainActivityContent() } // ←  для превью, нельзя с аргументами!
     }
 }
 
@@ -112,30 +104,5 @@ fun Header(image: Int, description: String) {
 fun TemperatureText(celsius: Int) {
     val fahrenheit = (celsius.toDouble()) * 9 / 5 + 32
     Text("$celsius is $fahrenheit Fahrenheit", color = Green, fontSize = 20.sp)
-}
-
-
-@Composable
-fun ChangeHello() {
-    var name by remember { mutableStateOf("friend") }
-
-    Column {
-        Text("Hello, $name!", color = Blue)
-        Button(onClick = { name = "Compose" }) {
-            Text("Change")
-        }
-    }
-
-}
-
-@Composable
-fun EnterTemperature(temp: String, changed: (String) -> Unit) {
-    TextField(
-        value = temp,
-        label = { Text("Enter a temperature in Celsius") },
-        onValueChange = changed,
-        textStyle = TextStyle(textAlign = TextAlign.Center),
-        modifier = Modifier.fillMaxWidth()
-    )
 }
 
